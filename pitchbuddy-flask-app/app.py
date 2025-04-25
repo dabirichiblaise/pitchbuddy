5fcfdac7ca00721b84a60a7c52a129d24adab7da
dabirichiblaise <dabirichiblaise@gmail.com>
2025-04-25 14:06:25 -0700

    Setup Interface

--- pitchbuddy-flask-app/app.py
+++ pitchbuddy-flask-app/app.py
@@ -1,12 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import url_for
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def welcome():
+    return 'Welcome to PitchBuddy!'
+
+@app.route('/match/<matchname>')
+def match(matchname): #what will matchname be? A URL that will link to matches in the database?
+    return f'Matches: {matchname} is not implemented yet.'
+    
+@app.route('/players/<matchname>')
+def players(matchname, players=None):
+    return f'{players} for fixture: {matchname} is not implemented yet.'
+
+with app.test_request_context():
+    print(url_for('welcome'))
+    print(url_for('match', matchname='example_match'))
+    print(url_for('players', matchname='example_match'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pitchbuddy-react-app/src/App.js
+++ pitchbuddy-react-app/src/App.js
@@ -2,17 +2,48 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [WelcomeMessage, setWelcomeMessage] = useState('');
+  const [Match, setMatch] = useState('');
+  const [Players, setPlayers] = useState('');
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000')
+    fetch('http://127.0.0.1:5000/')
     .then(response => response.text())
-    .then(data => setMessage(data));
+    .then(data => setWelcomeMessage(data))
+  }, []);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/match/matchname')
+    .then(response => response.text())
+    .then(data => setMatch(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/players/matchname')
+    .then(response => response.text())
+    .then(data => setPlayers(data));
   }, []);
 
   return (
     <div className="App">
-     <h1>{message}</h1>
+     <h1>{WelcomeMessage}</h1>
+
+     <nav className='navbar'>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/match">Match</a></li>
+        <li><a href="/players">Players</a></li>
+      </ul>
+     </nav>
+
+    <div className="match">
+      <h2>Matches</h2>
+      <p>{Match}</p>
+      </div>
+    <div className="players">
+      <h2>Players</h2>
+      <p>{Players}</p>
+      </div>
     </div>
   );
 }
